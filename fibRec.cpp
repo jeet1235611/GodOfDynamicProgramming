69593ea40928a854e0fda7bf24220b6d39bca14b
acoustic_jeet <js.jitendra11@gmail.com>
2018-07-15 07:59:43 +0530

    Add files via upload

---
+++ fibRec.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+
+//Base case.
+	if(n <= 1){
+		return 1;
+	}
+	
+
+//Recursive Case.	
+	//int first = fib(n-1);
+	//int second = fib(n-2);
+	//cout << first + second << endl;
+
+	//cout << fib(n-1) + fib(n-2) << endl;
+
+	//cout << fib(n) << endl;
+	
+	int smallOutput = fib(n-1) + fib(n-2);
+
+	return smallOutput;
+}
+
+int main() {
+	int n;
+	cout << "Enter the nth fibonacci number to be found out " << endl;
+	cin >> n;
+
+	cout << fib(n);
+}
